66b9ffd16eb70d5910bf63f75e012e46827b5483
Jayson Aquiles <jaysontedge@gmail.com>
2022-07-07 21:30:17 -0500

    ilustracionesTerminadas

---
+++ i.e. index.js
@@ -0,0 +1,24 @@
+function mostrar2() {
+    document.getElementById('container')
+    .style.display = 'block';
+} 
+
+function ocultar1() {
+    document.getElementById('body')
+    .style.display = 'none';
+} 
+
+function mostrar1() {
+    document.getElementById('body')
+    .style.display = 'block';
+}
+
+function ocultar2() {
+    document.getElementById('container')
+    .style.display = 'none';
+}
+
+function mostrar3() {
+    document.getElementById('Ilustracion3')
+    .style.display = 'block';
+}
