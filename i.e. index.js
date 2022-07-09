5b97e61ab6ed2910dfd4b03d14a1bc693173951a
Jayson Aquiles <jaysontedge@gmail.com>
2022-07-08 19:09:40 -0500

    commitFinal

--- i.e. index.js
+++ i.e. index.js
@@ -22,3 +22,18 @@ function mostrar3() {
     document.getElementById('Ilustracion3')
     .style.display = 'block';
 }
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault(); 
+    const inputValue = document.getElementById('verificacion').value; 
+    const errorMessage = document.querySelector('.error-message'); 
+    if(!inputValue) { 
+        errorMessage.classList.add('active'); 
+    }
+    else {
+        const paragraph = document.getElementById('parrafo3'); 
+        paragraph.innerText = `Tarjeta enviada a ${inputValue}!`; 
+        ocultar2();
+        mostrar3(); 
+    }
+})
